a0ae1953259fcc1d1ca8dc82ca09125dec2da234
Morgan <morgan.espindola@gmail.com>
2024-01-19 17:37:22 -0300

    Photo Gallery: First steps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import { photoList } from "@/data/photoList";
+
 export default function Home() {
   return (
-    <div className="w-screen h-screen">
-      <h1>Space Photos</h1>
-      <section className="container max-w-5xl mx-auto"></section>
+    <div className="mx-2">
+      <h1 className="text-center text-3xp font-bold my-10">Space Photos</h1>
+      <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {photoList.map((photo) => (
+          <div key={photo.id}>...</div>
+        ))}
+      </section>
     </div>
   );
 }
